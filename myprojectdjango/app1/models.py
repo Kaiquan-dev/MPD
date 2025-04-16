25b6be6c8c628f23862dfc991c9f6bbaa21902c5
Kaiquan-dev <football133@mail.ru>
2025-04-16 20:57:53 +0300

    izmenil3

--- myprojectdjango/app1/models.py
+++ myprojectdjango/app1/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 
 class Users(models.Model):
-    name = models.CharField(blank=False)
-    surname = models.CharField(blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    surname = models.CharField(max_length=100, blank=False)
     birthdate = models.DateField(default=0)
+
+    def __str__(self):
+        return f'{self.surname} {self.name}'
 
 
 class Book(models.Model):
-    title = models.CharField(blank=False)
+    title = models.CharField(max_length=100, blank=False)
     pages = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
     
